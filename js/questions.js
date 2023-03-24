9c4b16118686e2680576f181cc8a9273c4890908
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-03-24 14:12:27 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,57 +1,271 @@
 let questions = [
     {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
+    question: "Little interest or pleasure in doing things",
+    answer: "NOT AT ALL",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
     ]
   },
     {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question: "Feeling down, depressed, or hopeless",
+    answer: "NEARLY EVERY DAY",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
     ]
   },
     {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
+    question: "Trouble falling or staying asleep, or sleeping too much",
+    answer: "NEARLY EVERY DAY",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
     ]
   },
     {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
+    question: "Feeling tired or having little energy",
+    answer: "NEARLY EVERY DAY",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+       {
+    numb: 5,
+    question: "Poor appetite or overeating",
+    answer: "NOT AT ALL",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+   {
+    numb: 6,
+    question: "Feeling bad about yourself - or that you are a failure or have let yourself or your family down
+",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+   {
+    numb: 7,
+    question: "Trouble concentrating on things, such as reading the newspaper or watching television
+",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Moving or speaking so slowly that other people could have noticed Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
     ]
   },
     {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    numb: 9,
+    question: "Thoughts that you would be better off dead, or of hurting yourself",
+    answer: "NEARLY EVERYDAY",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
     ]
   },
+   {
+    numb: 10,
+    question: "If you checked off any problems, how difficult have these problems made it for you at work, home, or with other people?",
+    answer: "EXTREMELY DIFFICULT",
+    options: [
+    "NOT DIFFICULT AT ALL",
+
+    "SOMEWHAT DIFFICULT",
+
+    "VERY DIFFICULT",
+
+    "EXTREMELY DIFFICULT"
+    ]
+  },
+  {
+    numb: 11,
+    question: "I feel guilty",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+  {
+    numb: 12,
+    question: "I have difficulty making decisions",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+    {
+    numb: 13,
+    question: "I believe that nothing will ever work out for me",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+   {
+    numb: 14,
+    question: "I feel irritated",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+   {
+    numb: 15,
+    question: "I cry (or I feel like crying)",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+     {
+    numb: 16,
+    question: "I feel like I am alone (or want to be alone)",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+    {
+    numb: 17,
+    question: "It takes me a lot of effort to do the smallest of things",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+   {
+    numb: 18,
+    question: "I feel helpless",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  },
+   { numb: 19,
+    question: "I don’t feel happy even when good things happen",
+    answer: "All the time",
+    options: [
+    "Never",
+
+    "Hardly ever",
+
+    "Some of the time",
+
+    "All the time"
+    ]
+  }, 
+ 
+   
 ];
