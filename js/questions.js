a92ad2d9ccb48d4e85dfe44ee642585c924683e4
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-03-24 19:54:30 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,283 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Little interest or pleasure in doing things",
+    answer: "NOT AT ALL",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+   numb: 2,
+    question: "Feeling down, depressed, or hopeless",
+    answer: "NEARLY EVERY DAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Trouble falling or staying asleep, or sleeping too much",
+    answer: "NEARLY EVERY DAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Feeling tired or having little energy",
+    answer: "NEARLY EVERY DAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Poor appetite or overeating",
+    answer: "NOT AT ALL",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Feeling bad about yourself - or that you are a failure or have let yourself or your family down",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 7,
+    question: "Trouble concentrating on things, such as reading the newspaper or watching television",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 8,
+    question: "Moving or speaking so slowly that other people could have noticed Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 9,
+    question: "Thoughts that you would be better off dead, or of hurting yourself",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 10,
+    question: "If you checked off any problems, how difficult have these problems made it for you at work, home, or with other people?",
+    answer: "EXTREMELY DIFFICULT",
+    options: [
+    "NOT DIFFICULT AT ALL",
+
+    "SOMEWHAT DIFFICULT",
+
+    "VERY DIFFICULT",
+
+    "EXTREMELY DIFFICULT"
+    ]
+    },  
+{
+    numb: 11,
+    question: "Little interest or pleasure in doing things",
+    answer: "NOT AT ALL",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+   numb: 12,
+    question: "Feeling down, depressed, or hopeless",
+    answer: "NEARLY EVERY DAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Trouble falling or staying asleep, or sleeping too much",
+    answer: "NEARLY EVERY DAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 14,
+    question: "Feeling tired or having little energy",
+    answer: "NEARLY EVERY DAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 15,
+    question: "Poor appetite or overeating",
+    answer: "NOT AT ALL",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+  },
+    {
+    numb: 16,
+    question: "Feeling bad about yourself - or that you are a failure or have let yourself or your family down",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 17,
+    question: "Trouble concentrating on things, such as reading the newspaper or watching television",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 18,
+    question: "Moving or speaking so slowly that other people could have noticed Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 19,
+    question: "Thoughts that you would be better off dead, or of hurting yourself",
+    answer: "NEARLY EVERYDAY",
+    options: [
+    "NOT AT ALL",
+
+    "SEVERAL DAYS",
+
+    "MORE THAN HALF THE DAYS",
+
+    "NEARLY EVERY DAY"
+    ]
+    },
+      {
+    numb: 20,
+    question: "If you checked off any problems, how difficult have these problems made it for you at work, home, or with other people?",
+    answer: "EXTREMELY DIFFICULT",
+    options: [
+    "NOT DIFFICULT AT ALL",
+
+    "SOMEWHAT DIFFICULT",
+
+    "VERY DIFFICULT",
+
+    "EXTREMELY DIFFICULT"
+    ]
+    },  
+];
